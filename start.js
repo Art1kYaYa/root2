dc9f0a65e436d612b643320274f2af6c74b88cbc
Art1kYaYa <qwerty12345a1t@gmail.com>
2025-02-17 15:58:16 +0300

    Update start.js

--- start.js
+++ start.js
@@ -2,4 +2,4 @@ require('./twitch'); // Запуск Twitch-бота
 require('./index'); // Запуск Telegram-бота
 require('./test'); // Запуск Telegram-бота
 require('./psm'); // Запуск Telegram-бота
-require('./sait'); // Запуск Twitch-бота
+// require('./sait'); // Запуск Twitch-бота
